0fd60c0a69556185b2ba92986b4feeeadd29434e
Per Fredelius <per.fredelius@findwise.com>
2016-05-06 16:21:15 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import {
+  // These are the basic GraphQL types
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLEnumType,
+
+  // This is used to create required fields and arguments
+  GraphQLNonNull,
+
+  // This is the class we need to create the schema
+  GraphQLSchema,
+
+  // This function is used execute GraphQL queries
+  graphql
+} from 'graphql';
+
+
+
+
+const Query = new GraphQLObjectType({
+  name: 'RootQueries',
+  fields: () => ({
+    echo: {
+      type: new GraphQLNonNull(GraphQLString),
+      args: {
+        message: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(rootValue, args) {
+        return `received: ${args.message}`;
+      }
+    }
+  })
+});
+
+const Schema = new GraphQLSchema({
+  query: Query
+});
+
+
+let query = `
+  query getEcho($inputMessage: String!) {
+    receivedMessage: echo(message: $inputMessage)
+  }
+`;
+
+graphql(Schema, query, null, null, {inputMessage: "Hello"}).then(function(result) {
+  console.log(result);
+});
